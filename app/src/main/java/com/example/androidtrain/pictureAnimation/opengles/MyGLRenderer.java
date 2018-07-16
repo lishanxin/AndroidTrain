034c85d917a98b99982840ca36312448ec787c9b
lizhizhi <li540929968@163.com>
2018-07-16 22:59:05 +0800

    OpenGL ES20自动动画与触摸动画
    自动动画与触摸动画，主要时矩阵变换时输入角度的不同，导致动画显示模式的不同。需要自动动画时，获取系统时间并计算，并且需要将setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);注释掉，不使用。

--- app/src/main/java/com/example/androidtrain/pictureAnimation/opengles/MyGLRenderer.java
+++ app/src/main/java/com/example/androidtrain/pictureAnimation/opengles/MyGLRenderer.java
@@ -3,6 +3,7 @@ package com.example.androidtrain.pictureAnimation.opengles;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -16,10 +17,27 @@ class MyGLRenderer implements GLSurfaceView.Renderer{
     private Triangle mTriangle;
     private Square mSquare;
 
+    //定义投影
     //mMVPMatrix is an abbreviation for "Model View Projection Matrix"
     private final float[] mMVPMatrix = new float[16];
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
+
+    //为旋转所创建的变换矩阵
+    private float[] mRotationMatrix = new float[16];
+
+
+    //设置旋转角度
+    public volatile float mAngle;
+
+    public float getAngle(){
+        return mAngle;
+    }
+
+    public void setAngle(float angle){
+        mAngle = angle;
+    }
+
 
     public static int loadShader(int type, String shaderCode){
         // create a vertex shader type(GLES20.GL_VERTEX_SHADER)
@@ -49,6 +67,7 @@ class MyGLRenderer implements GLSurfaceView.Renderer{
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
 
+        //定义投影
         float ratio = (float) width/height;
 
         //this projection matrix is applied to object coordinates
@@ -61,6 +80,7 @@ class MyGLRenderer implements GLSurfaceView.Renderer{
         //Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
+        //正常显示
         // Set the camera position (View matrix)
         Matrix.setLookAtM(mViewMatrix, 0,0,0,-3,0f,0f,0f,0f,1.0f,0.0f);
 
@@ -68,6 +88,28 @@ class MyGLRenderer implements GLSurfaceView.Renderer{
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
         //Draw shape
-        mTriangle.draw(mMVPMatrix);
+//        mTriangle.draw(mMVPMatrix);
+
+        //自动旋转显示
+        float[] scratch = new float[16];
+
+        //自动旋转显示
+        //Create a totation transformation for the triangle
+//        long time = SystemClock.uptimeMillis() % 4000L;
+//        float angle = 0.090f * ((int) time);
+//        Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0,-1.0f);
+
+
+        //触碰旋转显示
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0,-1.0f);
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+        // Draw triangle
+        mTriangle.draw(scratch);
     }
+
+
 }
--- app/src/main/java/com/example/androidtrain/pictureAnimation/opengles/MyGLSurfaceView.java
+++ app/src/main/java/com/example/androidtrain/pictureAnimation/opengles/MyGLSurfaceView.java
@@ -2,13 +2,20 @@ package com.example.androidtrain.pictureAnimation.opengles;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
 
 /**
  * Created by lizz on 2018/7/15.
  */
 
-class MyGLSurfaceView extends GLSurfaceView {
+class MyGLSurfaceView extends GLSurfaceView{
     private final MyGLRenderer mRenderer;
+
+    //配置触摸监听器
+    private final float TOUCH_SCALE_FACTOR = 180.0F / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+
     public MyGLSurfaceView(Context context) {
         super(context);
 
@@ -20,7 +27,45 @@ class MyGLSurfaceView extends GLSurfaceView {
         //Set the Renderer for drawing on the GLSurfaceView
         setRenderer(mRenderer);
 
+        //为了能够让三角形旋转，需将此行代码注释掉，否者OpenGL只会对这个形状执行一次旋转
         // Render the view only when there is a change in the drawing data
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight()/2){
+                    dx = dx * -1;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth()/ 2){
+                    dy = dy * -1;
+                }
+
+                mRenderer.setAngle(
+                        mRenderer.getAngle() +
+                                ((dx + dy ) * TOUCH_SCALE_FACTOR));
+                requestRender();
+        }
+
+        mPreviousY = y;
+        mPreviousX = x;
+
+        return true;
     }
 }
