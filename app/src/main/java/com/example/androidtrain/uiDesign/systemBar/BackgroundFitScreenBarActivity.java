536346caaccc8f3c186b3738a37dcdb389f3d73f
lizhizhi <li540929968@163.com>
2018-09-09 00:39:36 +0800

    静态背景设置与适配导航栏变化
    android 启动页冷启动适配全面屏和虚拟键

---
+++ app/src/main/java/com/example/androidtrain/uiDesign/systemBar/BackgroundFitScreenBarActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidtrain.uiDesign.systemBar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidtrain.R;
+
+public class BackgroundFitScreenBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_background_fit_screen_bar);
+    }
+}
