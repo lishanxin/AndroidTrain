0319592ccd802e30b5258945cf7c79cd367ffe9e
lizhizhi <li540929968@163.com>
2018-09-10 15:19:22 +0800

    修改状态栏颜色

---
+++ app/src/main/java/com/example/androidtrain/WelcomeActivity.java
@@ -0,0 +1,53 @@
+package com.example.androidtrain;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by sx on 2018/9/10.
+ */
+
+public class WelcomeActivity extends Activity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(0x7fe23455);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            goToMainActivity();
+                            WelcomeActivity.this.finish();
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+
+    public void goToMainActivity(){
+        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
