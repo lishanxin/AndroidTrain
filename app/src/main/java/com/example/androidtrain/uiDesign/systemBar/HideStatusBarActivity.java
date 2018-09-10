7b6b63b22f839ebb7a8f4e6b46503aaf9c7840f1
lizhizhi <li540929968@163.com>
2018-09-10 23:51:33 +0800

    使内容显示在状态栏下方，调整隐藏状态栏的代码位置。
    TRAIN推荐的sdk>=16时所使用的隐藏状态栏的方式会出现bug，不建议使用

--- app/src/main/java/com/example/androidtrain/uiDesign/systemBar/HideStatusBarActivity.java
+++ app/src/main/java/com/example/androidtrain/uiDesign/systemBar/HideStatusBarActivity.java
@@ -5,31 +5,47 @@ import android.app.Activity;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 import com.example.androidtrain.R;
 
 public class HideStatusBarActivity extends Activity {
 
+    private static final String TAG = "HideStatusBar";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        hideLowerIceCream();
-        hideUperIceCream();
+        hideLowerIceCream();
         setContentView(R.layout.activity_hide_status_bar);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideUperIceCream();
+        //在标题栏下展示内容
+//        setContentUnderStatusBar();
     }
 
     //<!--4.0及以下使用设置主题来隐藏状态栏 android:theme="@android:style/Theme.Holo.NoActionBar.Fullscreen" -->
     //还可以通过以下方法来隐藏状态栏
     private void hideLowerIceCream(){
-        if (Build.VERSION.SDK_INT < 16){
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
     }
 
-    //在4.1及以上可使用此种方式来隐藏，
+    //在4.1及以上可使用此种方式来隐藏，这个方式不好用，使用hideLowerIceCream()方法来替代。
     /**
      * 需注意以下几点：
      * 1：一旦UI标签被清除(比如跳转到另一个Activity),如果你还想隐藏状态栏你就必须再次设定它
@@ -47,6 +63,8 @@ public class HideStatusBarActivity extends Activity {
             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
             decorView.setSystemUiVisibility(uiOptions);
 
+
+
             // Remember that you should never show the action bar if the
             // status bar is hidden, so hide that too if necessary.
             try {
@@ -59,8 +77,37 @@ public class HideStatusBarActivity extends Activity {
     }
 
 
+
+
     /**
      * 让内容显示在状态栏后面，以稳定布局。
+     * 这个放在onWindowFocusChanged里会出问题，放在onResume里正常。
      */
+    private void setContentUnderStatusBar(){
+        if (Build.VERSION.SDK_INT >= 16){
+            View decorView = getWindow().getDecorView();
+            //Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(uiOptions);
 
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            try {
+                ActionBar actionBar = getActionBar();
+                if (actionBar != null)actionBar.hide();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //全屏显示
+//        hideUperIceCream();
+        if (hasFocus){
+            Log.d(TAG, "SystemUiVisibility: " + getWindow().getDecorView().getSystemUiVisibility());
+        }
+    }
 }
