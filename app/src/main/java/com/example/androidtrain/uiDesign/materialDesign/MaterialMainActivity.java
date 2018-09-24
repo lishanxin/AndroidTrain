cd9fd4f0702c5f46f736ff81635f17b843ccd5a6
lizhizhi <li540929968@163.com>
2018-09-24 17:15:03 +0800

    动画——UI元素出现或隐藏
    ViewAnimationUtils.createCircularReveal

--- app/src/main/java/com/example/androidtrain/uiDesign/materialDesign/MaterialMainActivity.java
+++ app/src/main/java/com/example/androidtrain/uiDesign/materialDesign/MaterialMainActivity.java
@@ -1,13 +1,19 @@
 package com.example.androidtrain.uiDesign.materialDesign;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.transition.Explode;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.androidtrain.R;
@@ -19,12 +25,16 @@ import com.example.androidtrain.R;
 public class MaterialMainActivity extends Activity {
 
     TextView mainTextView;
+    CardView mCardView;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_main);
 
+        mButton = (Button) findViewById(R.id.main_material_button);
+        mCardView = (CardView) findViewById(R.id.main_material_cardview);
         mainTextView = (TextView) findViewById(R.id.main_material_text);
         mainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +43,18 @@ public class MaterialMainActivity extends Activity {
                 mainTextView.setTranslationZ(100);
 
                 onSomeButtonClicked();
+            }
+        });
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 21){
+                    if (mCardView.getVisibility() == View.VISIBLE){
+                        animateInvisible(mCardView);
+                    }else {
+                        animateVisible(mCardView);
+                    }
+                }
             }
         });
     }
@@ -52,5 +74,55 @@ public class MaterialMainActivity extends Activity {
         }else {
             startActivity(intent);
         }
+    }
+
+    public void animateVisible(final View view){
+        //view is previously invisible view
+
+        //get the center for the clipping circle
+        int cx = (view.getLeft() + view.getRight()) / 2;
+        int cy = (view.getTop() + view.getBottom()) / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = view.getWidth();
+
+        // create and start the animator for this view
+        // (the start radius is zero)
+        Animator anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, finalRadius);
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                view.setVisibility(View.VISIBLE);
+            }
+        });
+        anim.start();
+    }
+
+    public void animateInvisible(final View view){
+        // previously visible view
+
+        //get the center for the clipping circle
+        int cx = (view.getLeft() + view.getRight()) / 2;
+        int cy = (view.getTop() + view.getBottom()) / 2;
+
+        // get the initial radius for the clipping circle
+        int initialRadius = view.getWidth();
+
+        // create the animation (the final radius is zero)
+        Animator anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, initialRadius, 0);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                view.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // start the animation
+        anim.start();
     }
 }
