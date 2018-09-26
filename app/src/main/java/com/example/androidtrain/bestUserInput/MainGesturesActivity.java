7d908198638c589517189d17b5927b384087ac43
lizhizhi <li540929968@163.com>
2018-09-26 13:34:45 +0800

    手势监听

--- app/src/main/java/com/example/androidtrain/bestUserInput/MainGesturesActivity.java
+++ app/src/main/java/com/example/androidtrain/bestUserInput/MainGesturesActivity.java
@@ -1,19 +1,25 @@
 package com.example.androidtrain.bestUserInput;
 
+import android.app.Activity;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.androidtrain.R;
 
-public class MainGesturesActivity extends AppCompatActivity {
+public class MainGesturesActivity extends Activity
+        implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 
     private static final String TAG = "MainGesturesA";
     private static final String TAGB = "MainGesturesB";
+
+    private GestureDetectorCompat mDetector;
 
     private Button mTestButton1;
 
@@ -22,31 +28,19 @@ public class MainGesturesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_gestures);
 
+        // Instantiate the gesture detector with the
+        // application context and an implementation of
+        // GestureDetector.OnGestureListener
+        mDetector = new GestureDetectorCompat(this, this);
+        // Set the gesture detector as the double tap
+        // listener
+        mDetector.setOnDoubleTapListener(this);
+
         mTestButton1 = (Button) findViewById(R.id.main_gestures_button_test1);
         mTestButton1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                int action = event.getActionMasked();
-                switch (action){
-                    case MotionEvent.ACTION_DOWN:
-                        Log.d(TAGB, "Action was DOWN");
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        Log.d(TAGB, "Action was MOVE");
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        Log.d(TAGB, "Action was UP");
-                        return true;
-                    case MotionEvent.ACTION_CANCEL:
-                        Log.d(TAGB, "Action was CANCEL");
-                        return true;
-                    case MotionEvent.ACTION_OUTSIDE:
-                        Log.d(TAGB, "Action was OUTSIDE" +
-                                "Movement occurred outside bounds of current screen element");
-                        return true;
-                    default:
-                        return false;
-                }
+                return false;
             }
         });
     }
@@ -64,27 +58,175 @@ public class MainGesturesActivity extends AppCompatActivity {
      */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getActionMasked();
-        switch (action){
-            case MotionEvent.ACTION_DOWN:
-                Log.d(TAG, "Action was DOWN");
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "Action was MOVE");
-                return true;
-            case MotionEvent.ACTION_UP:
-                Log.d(TAG, "Action was UP");
-                return true;
-            case MotionEvent.ACTION_CANCEL:
-                Log.d(TAG, "Action was CANCEL");
-                return true;
-            case MotionEvent.ACTION_OUTSIDE:
-                Log.d(TAG, "Action was OUTSIDE" +
-                "Movement occurred outside bounds of current screen element");
-                return true;
-            default:
-                return super.onTouchEvent(event);
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+//        int action = event.getActionMasked();
+//        switch (action){
+//            case MotionEvent.ACTION_DOWN:
+//                Log.d(TAG, "Action was DOWN");
+//                return true;
+//            case MotionEvent.ACTION_MOVE:
+//                Log.d(TAG, "Action was MOVE");
+//                return true;
+//            case MotionEvent.ACTION_UP:
+//                Log.d(TAG, "Action was UP");
+//                return true;
+//            case MotionEvent.ACTION_CANCEL:
+//                Log.d(TAG, "Action was CANCEL");
+//                return true;
+//            case MotionEvent.ACTION_OUTSIDE:
+//                Log.d(TAG, "Action was OUTSIDE" +
+//                "Movement occurred outside bounds of current screen element");
+//                return true;
+//            default:
+//                return super.onTouchEvent(event);
+//
+//        }
+    }
 
+    /**
+     * Notified when a single-tap occurs.
+     * <p>
+     * Unlike {OnGestureListener#onSingleTapUp(MotionEvent)}, this
+     * will only be called after the detector is confident that the user's
+     * first tap is not followed by a second tap leading to a double-tap
+     * gesture.
+     *
+     * @param e The down motion event of the single-tap.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        Log.d(TAG, "onSingleTapConfirmed: " + e.toString());
+        return true;
+    }
+
+    /**
+     * Notified when a double-tap occurs.
+     *
+     * @param e The down motion event of the first tap of the double-tap.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Log.d(TAG, "onDoubleTap: " + e.toString());
+        return true;
+    }
+
+    /**
+     * Notified when an event within a double-tap gesture occurs, including
+     * the down, move, and up events.
+     *
+     * @param e The motion event that occurred during the double-tap gesture.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        Log.d(TAG, "onDoubleTapEvent: " + e.toString());
+        return true;
+    }
+
+    /**
+     * Notified when a tap occurs with the down {@link MotionEvent}
+     * that triggered it. This will be triggered immediately for
+     * every down event. All other events should be preceded by this.
+     *
+     * @param e The down motion event.
+     */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.d(TAG, "onDown: " + e.toString());
+        return true;
+    }
+
+    /**
+     * The user has performed a down {@link MotionEvent} and not performed
+     * a move or up yet. This event is commonly used to provide visual
+     * feedback to the user to let them know that their action has been
+     * recognized i.e. highlight an element.
+     *
+     * @param e The down motion event
+     */
+    @Override
+    public void onShowPress(MotionEvent e) {
+        Log.d(TAG, "onShowPress: " + e.toString());
+    }
+
+    /**
+     * Notified when a tap occurs with the up {@link MotionEvent}
+     * that triggered it.
+     *
+     * @param e The up motion event that completed the first tap
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.d(TAG, "onSingleTapUp: " + e.toString());
+        return true;
+    }
+
+    /**
+     * Notified when a scroll occurs with the initial on down {@link MotionEvent} and the
+     * current move {@link MotionEvent}. The distance in x and y is also supplied for
+     * convenience.
+     *
+     * @param e1        The first down motion event that started the scrolling.
+     * @param e2        The move motion event that triggered the current onScroll.
+     * @param distanceX The distance along the X axis that has been scrolled since the last
+     *                  call to onScroll. This is NOT the distance between {@code e1}
+     *                  and {@code e2}.
+     * @param distanceY The distance along the Y axis that has been scrolled since the last
+     *                  call to onScroll. This is NOT the distance between {@code e1}
+     *                  and {@code e2}.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.d(TAG, "onScroll: " + e1.toString()+e2.toString());
+        return false;
+    }
+
+    /**
+     * Notified when a long press occurs with the initial on down {@link MotionEvent}
+     * that trigged it.
+     *
+     * @param e The initial on down motion event that started the longpress.
+     */
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.d(TAG, "onLongPress: " + e.toString());
+    }
+
+    /**
+     * Notified of a fling event when it occurs with the initial on down {@link MotionEvent}
+     * and the matching up {@link MotionEvent}. The calculated velocity is supplied along
+     * the x and y axis in pixels per second.
+     *
+     * @param e1        The first down motion event that started the fling.
+     * @param e2        The move motion event that triggered the current onFling.
+     * @param velocityX The velocity of this fling measured in pixels per second
+     *                  along the x axis.
+     * @param velocityY The velocity of this fling measured in pixels per second
+     *                  along the y axis.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.d(TAG, "onFling: " + e1.toString()+e2.toString());
+        return true;
+    }
+
+    public class GestureOfMine extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(TAG, "onFling: " + e1.toString()+e2.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.d(TAG, "onDown: " + e.toString());
+            return true;
         }
     }
 }
